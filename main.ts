318d8f59a582d7c986d8555b8c7fc539811ed36b
TheMaksoo <39918032+Moheax@users.noreply.github.com>
2021-10-12 15:16:53 +0200

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -2,7 +2,14 @@ input.onButtonPressed(Button.A, function () {
     pong = game.createSprite(0, randint(0, 4))
     pongshadow = game.createSprite(0, pong.get(LedSpriteProperty.Y))
     pongshadow.change(LedSpriteProperty.Brightness, -70)
-    move()
+    for (let index = 0; index < 4; index++) {
+        pong.change(LedSpriteProperty.X, 1)
+        basic.pause(500)
+        pongshadow.change(LedSpriteProperty.X, 1)
+    }
+    pong.delete()
+    radio.sendValue("pongy", pong.get(LedSpriteProperty.Y))
+    pongshadow.delete()
 })
 radio.onReceivedValue(function (name, value) {
     if (name == "pongy") {
@@ -38,18 +45,19 @@ radio.onReceivedValue(function (name, value) {
         }
         pong.delete()
         pongshadow.delete()
+        pong = game.createSprite(0, randint(0, 4))
+        pongshadow = game.createSprite(0, pong.get(LedSpriteProperty.Y))
+        pongshadow.change(LedSpriteProperty.Brightness, -70)
+        for (let index = 0; index < 4; index++) {
+            pong.change(LedSpriteProperty.X, 1)
+            basic.pause(500)
+            pongshadow.change(LedSpriteProperty.X, 1)
+        }
+        pong.delete()
+        radio.sendValue("pongy", pong.get(LedSpriteProperty.Y))
+        pongshadow.delete()
     }
 })
-function move () {
-    for (let index = 0; index < 4; index++) {
-        pong.change(LedSpriteProperty.X, 1)
-        basic.pause(500)
-        pongshadow.change(LedSpriteProperty.X, 1)
-    }
-    pong.delete()
-    radio.sendValue("pongy", pong.get(LedSpriteProperty.Y))
-    pongshadow.delete()
-}
 let pongshadow: game.LedSprite = null
 let pong: game.LedSprite = null
 radio.setGroup(1)
